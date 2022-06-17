12aded79d587075c1b64bfbaf703a1d95d62e673
SuryanshShahi <suryansh06shahi@gmail.com>
2022-06-17 18:34:04 +0530

    login form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-import Home from "./Home";
+import Techno from "./techno";
 import "./App.css";
 
 const App = () => {
   return (
     <section>
       <div className="app">
-        <Home />
+        <Techno />
       </div>
     </section>
   );
